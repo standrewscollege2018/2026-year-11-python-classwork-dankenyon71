83de57eff64d5f145d45c3631a207f5a1846e362
dankenyon71 <dke4592@stacmail.net>
2026-02-05 11:58:15 +1300

    Create first-file.py

---
+++ first-file.py
@@ -0,0 +1,14 @@
+'''This program demonstrates print(), data types, variable, inputs and f-string'''
+import time
+import sys
+
+#print() is a function that outputs whatever is inside the brackets
+#numbers don't need speech marks
+
+print(67676767676767676767)
+
+#words need speech marks which make it a string
+print("SIIIIIX SEVVEEEEEN")
+
+print(f"Took {time.process_time()} seconds to run.")
+sys.exit()
