86ce2c7b7d454828c126a21a08c8305d24fd2948
dankenyon71 <dke4592@stacmail.net>
2026-02-12 12:31:06 +1300

    blackjack shuffle
    added a reshuffle function to blackjack, made a minor change to paracetamol task

--- paracetamol.py
+++ paracetamol.py
@@ -1,9 +1,8 @@
-age = int(input("What is your age?"))
-weight = float(input("What is your weight?"))
+age = int(input("What is your age? ")) and weight = float(input("What is your weight? "))
 
-if age >= 12 and age < 130 and weight > 0:
-    print("Take 2 500mg tablets")
-elif age > 0 and age < 130 and weight > 0:
+if age >= 12 and age < 130 and weight > 0 or weight <= 100:
+    print("Take 2 500mg tablets.")
+elif age > 0 and age < 130 and weight > 0 and weight < 200:
     print(f"Recommended dosage is {weight*10} milligrams.")
 else:
-    print("Age/weight is impossible")
+    print("Age/weight is impossible.")
